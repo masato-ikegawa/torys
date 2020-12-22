32edfc0f910daeb1ae341f58137527f2ee571764
吉井駿一 <yoshiishunichi@yoshiishunichinoMacBook-Pro.local>
2020-12-22 23:24:59 +0900

    yoshii first commit

---
+++ frontend/hack/src/components/Button.vue
@@ -0,0 +1,59 @@
+<template>
+  <button>{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    label: String,
+  },
+};
+</script>
+
+<style scoped>
+button {
+  user-select: none;
+  width: 280px;
+  height: 40px;
+  font-size: 13px;
+  cursor: pointer;
+  border: solid 1px #000;
+  border-radius: 10px;
+  background: rgb(249, 142, 18);
+  font-weight: bold;
+  color: #fff;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+button:focus {
+  outline: 0;
+}
+
+@media screen and (min-width: 600px) {
+  button {
+    width: 280px;
+    height: 40px;
+    font-size: 13px;
+  }
+}
+
+@media screen and (min-width: 900px) {
+  button {
+    width: 350px;
+    height: 40px;
+    font-size: 15px;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  button {
+    width: 400px;
+    height: 50px;
+    font-size: 17px;
+  }
+}
+</style>
---
+++ frontend/hack/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer>
+    <p>2020 TORYS-HIGH</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+footer {
+  width: 100%;
+  height: 70px;
+  background: #aaa;
+  position: absolute;
+  bottom: 0;
+}
+
+p {
+  margin: 0;
+  height: 100%;
+  font-size: 12px;
+  line-height: 70px;
+  color: white;
+  font-weight: bold;
+}
+
+@media screen and (min-width: 600px) {
+  p {
+    font-size: 15px;
+  }
+}
+
+@media screen and (min-width: 900px) {
+  p {
+    font-size: 18px;
+  }
+}
+</style>
