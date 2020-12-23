7290256ac8c2a19c6ab543f265f1a270d29b5068
aymp <nabenotomo.1093@gmail.com>
2020-12-24 01:11:51 +0900

    推論コードbackend/gan-sample/eval_mnist_gan完成（サーバーでちゃんと動くかな？） #2

--- backend/gan-sample/eval_mnist_gan.py
+++ backend/gan-sample/eval_mnist_gan.py
@@ -9,16 +9,31 @@ import torchvision
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from model_mnist_gan import *
 
-netD = Discriminator()
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE' #OMP: Error #15
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
 netG = Generator()
 
 #学習結果を読み込んで、
-netD.load_state_dict(torch.load('./netD.pth', map_location=lambda storage, loc: storage))
 netG.load_state_dict(torch.load('./netG.pth', map_location=lambda storage, loc: storage))
 
-#推論モードに設定（Dropout、BN、他の無効化）.to(device)は12/04追加。
-netD.eval()
-netG.eval()
+#推論モードに設定（Dropout、BN、他の無効化）
+netG.eval().to(device)
+
+#ランダムなベクトル
+fixed_noise = torch.randn(64, 100, device=device)
+
+fake = netG(fixed_noise)#.detach().cpu()
+
+fig = plt.figure(figsize=(15,15))
+for i in range(fake.shape[0]):
+    ax = fig.add_subplot(8,8,i+1)
+    ax.imshow(fake.detach().cpu().numpy()[i],cmap='gray')
+fig.savefig('generated_img.png')
+
