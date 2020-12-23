e2f64a1ab12603c6ecfc9cefa4f0e6b3d0e3ba0b
吉井駿一 <yoshiishunichi@yoshiishunichinoMacBook-Pro.local>
2020-12-24 02:19:01 +0900

    ツイートボタンコンポーネントに結果ツイートのプロパティを追加 #9

--- frontend/hack/src/App.vue
+++ frontend/hack/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container">
     <h1>ニコニコおみくじ</h1>
-    <Button class="btn" label="おみくじを回す" @click="tappedBtn"></Button>
-    <Loading class="loading"></Loading>
+    <Button class="btn disp" label="おみくじを回す" @click="tappedBtn"></Button>
+    <Button class="btn nodisp" label="もう1度回す" @click="tappedBtn"></Button>
+    <Loading class="loading nodisp"></Loading>
+    <TweetButton after=true :result="resultValue" class="tweet nodisp"></TweetButton>
     <Footer />
   </div>
 </template>
@@ -11,6 +13,7 @@
 import Button from "./components/Button.vue";
 import Loading from "./components/Loading.vue";
 import Footer from "./components/Footer.vue";
+import TweetButton from "./components/TweetButton.vue";
 
 function omikuji() {
   var random = Math.floor(Math.random() * 16);
@@ -23,32 +26,62 @@ export default {
     Button,
     Loading,
     Footer,
+    TweetButton
+  },
+  data() {
+    return {
+      resultValue: "",
+    }
   },
   methods: {
     tappedBtn() {
       this.$el.children[0].textContent = "Now Loading...";
+
+      this.$el.children[1].classList.remove("disp");
       this.$el.children[1].classList.add("nodisp");
-      this.$el.children[2].classList.add("disp");
+
+      this.$el.children[2].classList.remove("disp");
+      this.$el.children[2].classList.add("nodisp");
+
+      this.$el.children[3].classList.remove("nodisp");
+      this.$el.children[3].classList.add("disp");
+
+      this.$el.children[4].classList.remove("disp");
+      this.$el.children[4].classList.add("nodisp");
 
       var result = omikuji();
 
       setTimeout(() => {
         if (result < 1) {
           this.$el.children[0].textContent = "大凶…";
+          this.$data.resultValue = "大凶"
         } else if (result < 3) {
           this.$el.children[0].textContent = "凶…";
+          this.$data.resultValue = "凶"
         } else if (result < 6) {
           this.$el.children[0].textContent = "末吉";
+          this.$data.resultValue = "末吉"
         } else if (result < 10) {
           this.$el.children[0].textContent = "小吉";
+          this.$data.resultValue = "小吉"
         } else if (result < 13) {
           this.$el.children[0].textContent = "中吉";
+          this.$data.resultValue = "中吉"
         } else if (result < 15) {
           this.$el.children[0].textContent = "吉！";
+          this.$data.resultValue = "吉"
         } else {
           this.$el.children[0].textContent = "大吉！";
+          this.$data.resultValue = "大吉"
         }
-        this.$el.children[2].classList.add("nodisp");
+        this.$el.children[2].classList.remove("nodisp");
+        this.$el.children[2].classList.add("disp");
+
+        this.$el.children[3].classList.remove("disp");
+        this.$el.children[3].classList.add("nodisp");
+
+        this.$el.children[4].classList.remove("nodisp");
+        this.$el.children[4].classList.add("disp");
       }, 1000);
     },
   },
@@ -84,18 +117,26 @@ h1 {
 
 .btn {
   margin-top: 80px;
+  margin-right: auto;
+  margin-left: auto;
 }
 
 .loading {
   display: none;
 }
 
+.tweet{
+  margin-top: 30px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
 .disp {
-  display: block;
+  display: block !important;
 }
 
 .nodisp {
-  display: none;
+  display: none !important;
 }
 
 @media screen and (min-width: 600px) {
--- frontend/hack/src/components/TweetButton.vue
+++ frontend/hack/src/components/TweetButton.vue
@@ -1,9 +1,9 @@
 <template>
   <a
-    href="http://twitter.com/share?url=http://localhost:8080/&text=ニコニコおみくじ"
     id="tweetbutton"
     target="_blank"
     rel="noopener"
+    :href="href()"
     >Tweet</a
   >
 </template>
@@ -11,6 +11,25 @@
 <script>
 export default {
   name: "TweetButton",
+  props:{
+    after: {type:Boolean, default:false},
+    result: {type:String, default:""},
+  },
+  methods: {
+    href:function() {
+      const url = "http://localhost:8080/"
+
+      if (this.after && (this.result != "")){
+        const result = this.result
+        const link = "http://twitter.com/share?url="+url+"&text=ニコニコおみくじで" + result + "が出ました"
+        return link
+      }
+      else{
+        const link = "http://twitter.com/share?url="+ url +"&text=ニコニコおみくじ"
+        return link
+      }
+    }
+  },
 };
 </script>
 
