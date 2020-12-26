c6d249f41cdf4db16575f67b029578e1aa65235b
吉井駿一 <yoshiishunichi@yoshiishunichinoMacBook-Pro.local>
2020-12-26 09:35:28 +0900

    [WIP]imageを受け取るボタン

---
+++ frontend/hack/src/components/ImageButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <input type="file" accept="image/*" @change="fileChange($event)"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageButton",
+  data: {
+    imageData: "",
+  },
+  methods: { 
+    // ファイル読み込み
+    fileChange(e){
+      const files = e.target.files;
+      // ファイルが
+      if (files.length > 0){
+        const file = files[0];
+        const reader =  new FileReader();
+        reader.onload = (e) =>{
+          this.imageData = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  },
+};
+</script>
