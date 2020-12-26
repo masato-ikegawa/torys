278f5c0ba7d81f13f8e951d82c71770c1498052a
吉井駿一 <yoshiishunichi@yoshiishunichinoMacBook-Pro.local>
2020-12-26 12:55:12 +0900

    [WIP]画像を送るやつのテスト #11

--- frontend/hack/src/components/ImageButton.vue
+++ frontend/hack/src/components/ImageButton.vue
@@ -3,19 +3,42 @@
     <h4>顔写真を選んでください</h4>
     <input type="file" accept="image/*" @change="fileChange($event)" />
     <img :src="imageData" v-if="imageData" />
+    <button @click="tappedPostButton()" v-if="imageData">この画像に決定</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "ImageButton",
   components: {},
   data() {
     return {
       imageData: "",
+      data: {
+        bpi: null,
+      },
     };
   },
+  mounted: function () {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(
+        function (response) {
+          //デバッグ用にconsoleに出力
+          console.log(response.data.bpi);
+          this.bpi = response.data.bpi;
+        }.bind(this)
+      )
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
   methods: {
+    tappedPostButton() {
+      return axios.post("localhost:8080", { チンポ: String });
+    },
     // ファイル読み込み
     fileChange(e) {
       const files = e.target.files;
