f91f690854ad0a2cc441dce5b550437584be1f6b
吉井駿一 <yoshiishunichi@yoshiishunichinoMacBook-Pro.local>
2020-12-26 10:29:03 +0900

    imageを受け取るボタン作成

--- frontend/hack/src/components/ImageButton.vue
+++ frontend/hack/src/components/ImageButton.vue
@@ -1,29 +1,74 @@
 <template>
-  <div class="container">
-    <input type="file" accept="image/*" @change="fileChange($event)"/>
+  <div class="image-container">
+    <h4>顔写真を選んでください</h4>
+    <input type="file" accept="image/*" @change="fileChange($event)" />
+    <img :src="imageData" v-if="imageData" />
+    <Button label="この画像にする" v-if="imageData" />
   </div>
 </template>
 
 <script>
+import Button from "./Button.vue";
+
 export default {
   name: "ImageButton",
-  data: {
-    imageData: "",
+  components:{
+    Button,
   },
-  methods: { 
+  data() {
+    return {
+      imageData: "",
+    };
+  },
+  methods: {
     // ファイル読み込み
-    fileChange(e){
+    fileChange(e) {
       const files = e.target.files;
-      // ファイルが
-      if (files.length > 0){
+      // ファイルが選択されたかチェック
+      if (files.length > 0) {
         const file = files[0];
-        const reader =  new FileReader();
-        reader.onload = (e) =>{
+        // ファイルリーダーを使って画像を読み込み
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          // imageDataに格納
           this.imageData = e.target.result;
         };
         reader.readAsDataURL(file);
       }
-    }
+    },
   },
 };
 </script>
+
+<style scoped>
+.image-container {
+  width: 100%;
+  height: 300px;
+  padding: 0;
+}
+h4{
+  font-size: 15px;
+  padding-top: 6px;
+  margin-bottom: 12px;
+}
+img {
+  display: block;
+  width: 200px;
+  height: auto;
+  max-height: 500px;
+  margin: 0 auto;
+}
+
+@media screen and (min-width: 900px) {
+  img {
+    width: 250px;
+    height: auto;
+    max-height: 500px;
+  }
+  h4{
+    font-size: 17px;
+    padding-top: 9px;
+    margin-bottom: 18px;
+  }
+}
+</style>
