8198d375d9be43923a873d3f687682a1cb6991d2
吉井駿一 <syun_syun01@icloud.com>
2020-12-27 14:57:46 +0900

    エラーあ

--- frontend/hack/src/App.vue
+++ frontend/hack/src/App.vue
@@ -141,7 +141,7 @@ export default {
         })
         .catch((err) => {
           console.log(err);
-          this.$refs.omikujiRef.err();
+          this.$refs.omikujiRef.err(this.resultText);
         });
     },
   },
